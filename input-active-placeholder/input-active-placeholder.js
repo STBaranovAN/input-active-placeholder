a0e9fc1c86ca3af862aab24be6a28a6587c2f3d4
ST_DOMAIN\Baranov_AN <Baranov_AN@st.by>
2018-10-09 17:21:50 +0300

    Added input active placeholder project.

---
+++ input-active-placeholder/input-active-placeholder.js
@@ -0,0 +1,51 @@
+﻿(function($) {
+	var methods = {
+		init: function(options) {
+			var $element = $(this).last();
+			options = $.extend({}, options);
+			$element.addClass('text_input');
+			var placeholder = $element.attr('placeholder');
+			if (placeholder) {
+				$('<span>' + placeholder + '</span>').attr('class', 'placeholder').insertAfter($element);
+				$element.removeAttr('placeholder');
+			}
+			if (!isEmptyInput($element)) {
+				$element.addClass('input_full');
+			} else {
+				$element.removeClass('input_full');
+			}
+
+			var funcInputValueChanged = function(e) {
+				if (!isEmptyInput($element)) {
+					$element.addClass('input_full');
+				} else {
+					$element.removeClass('input_full');
+				}
+			};
+
+			if (!options.isCurrencyInput) {
+				$element.on('blur change viewModelUpdated', funcInputValueChanged);
+			}
+		},
+
+	};
+
+	function isEmptyInput($input) {
+		if ($input.val() == '')
+			return true;
+
+		return false;
+	};
+
+	var biaActivePlaceholder = function(method) {
+		if (methods[method]) {
+			return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+		} else if (typeof method === 'object' || !method) {
+			return methods.init.apply(this, arguments);
+		} else {
+			$.error('Method ' + method + ' does not exist on bia.jquery.input-active-placeholder');
+		}
+	};
+
+	$.fn.biaActivePlaceholder = biaActivePlaceholder;
+})(jQuery);
